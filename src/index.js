ae7f7078aded2226588afe146420871052f573db
hannan-emily <emilyhannan@gmail.com>
2018-03-08 16:37:34 -0800

    refactored and added components

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Post from './App';
+import './App.css';
+import Home from './components/Home';
 import registerServiceWorker from './registerServiceWorker';
 
-var post = {
-  title: 'Dinosaurs Are Extra-Awesome',
-
-  body: 'Boy, was Ragnor vicious!',
-  authors: [
-    "Finnegan Swarty",
-    "Andoliza Swanson",
-    "Marcule De Fin"
-  ],
-  comments: [
-    'Ragnor loved raspberry and blueberry tarts.',
-    'He did not care for lemon pie.',
-    'What a guy ... said everyone.',
-    "There once was a dinosaur who liked tarts. Ragnor said, See the master of arts! It was rather apple, But not very appel, And he could not resist the ace of hearts."
-  ]
-}
 
 
-ReactDOM.render
-  (<Post
-  title={post.title}
 
-  body={post.body}
-  comments={post.comments}
-  authors={post.authors}
-  />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
